7770fcde55955e638710f8fbe71ca8bb33aa8eb8
Colton Carner <ccarner13@gmail.com>
2019-08-27 21:21:43 +1000

    adding complete react files

---
+++ my-test-app/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import InformationPage from "./components/information";
+import NavBar from "./components/navbar";
+import QuizPage from "./components/quizzes";
+import HomePage from "./components/home";
+
+// this is the basic component that's responsible for rendering the default
+// starting view with the spinning react logo
+
+// babel can take this JSX (markup+js) into traditional JS for browsers
+// components will always use JSX, and then babel turns that into react.createelement staements
+//
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          {/* use a switch so we only render max of ONE of these pages */}
+          <Route path="/" exact component={HomePage} />
+          <Route path="/information" component={InformationPage} />
+          <Route path="/quizzes" component={QuizPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-test-app/src/components/quiz/quizService.jsx
@@ -0,0 +1,41 @@
+const qBank = [
+  {
+    question: "1+1=?",
+    answers: ["1", "2", "3", "4"],
+    correct: "2",
+    questionId: "01"
+  },
+  {
+    question: "2+1=?",
+    answers: ["1", "2", "3", "4"],
+    correct: "3",
+    questionId: "02"
+  },
+  {
+    question: "2+2=?",
+    answers: ["1", "2", "3", "4"],
+    correct: "4",
+    questionId: "03"
+  },
+  {
+    question: "1+0=?",
+    answers: ["1", "2", "3", "4"],
+    correct: "1",
+    questionId: "04"
+  },
+  {
+    question: "3+1=?",
+    answers: ["1", "2", "3", "4"],
+    correct: "4",
+    questionId: "04"
+  },
+  {
+    question: "0+0=?",
+    answers: ["0", "2", "3", "4"],
+    correct: "0",
+    questionId: "05"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
