6393859657088558e6f08678a6889f32d2af904a
Colton Carner <ccarner13@gmail.com>
2019-09-03 23:35:55 +1000

    db.js working

--- my-test-app/src/components/home.jsx
+++ my-test-app/src/components/home.jsx
@@ -3,18 +3,30 @@ import { Link } from "react-router-dom";
 import ImageDatabase from "../db.js";
 
 class HomePage extends Component {
-  state = {
-    mainMenu: [
-      { name: "Quizzes", link: "/quizzes" },
-      { name: "More about FES", link: "/information" },
-      { name: "Review Knowledge", link: "/review" }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainMenu: [
+        { name: "Quizzes", link: "/quizzes" },
+        { name: "More about FES", link: "/information" },
+        { name: "Review Knowledge", link: "/review" }
+      ],
+      imageURL: ""
+    };
+  }
+
+  componentDidMount() {
+    // NOTE had to use arrow function to ensure that 'this' binding was still to
+    // the outer class...
+    ImageDatabase.getImage("https://picsum.photos/200").then(imageString => {
+      this.setState({ imageURL: imageString });
+    });
+  }
+
   render() {
-    const data = ImageDatabase.getImage("https://via.placeholder.com/150");
     return (
       <React.Fragment>
-        <img alt="" src={`data:image/jpeg;base64,${data}`} />
+        <img alt="" src={`data:image/jpeg;base64,${this.state.imageURL}`} />
         <h1>Home</h1>
         {this.state.mainMenu.map(option =>
           this.renderMenuOption(option.name, option.link)
