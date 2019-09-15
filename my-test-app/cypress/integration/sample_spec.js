996c21fd10269327f9b66974d800f23c5a89731e
limg2 <limg2@student.unimelb.edu.au>
2019-09-15 20:42:53 +1000

    added some tests, need to make the test wait while video plays

--- my-test-app/cypress/integration/sample_spec.js
+++ my-test-app/cypress/integration/sample_spec.js
@@ -1,7 +1,24 @@
 describe('My First Test', function() {
-    it('visits app', function() {
+    it('views information', function() {
         cy.visit('http://localhost:3000/')
         cy.contains('Fire Safety Information').click()
+        cy.url().should('include', '/safetyHome')
      // expect(true).to.equal(true)
     })
+    it('takes quiz with video and gets results', function() {
+      cy.visit('http://localhost:3000/')
+      cy.contains('Quizzes').click()
+      cy.url().should('include', '/quizzes')
+      cy.contains('Next').click()
+      cy.get('video').click()
+    })
+   // expect(true).to.equal(true)
+    it('has working home button', function() {
+      cy.visit('http://localhost:3000/')
+      cy.contains('Quizzes').click()
+      cy.url().should('include', '/quizzes')
+      cy.contains('Home').click()
+      cy.url().should('eq', 'http://localhost:3000/')
+    
+  })
   })
