81852bd700033ea0676fc52570de46709f575614
Colton Carner <ccarner13@gmail.com>
2019-09-24 22:16:33 +1000

    didn't save changes from conflcits

--- my-test-app/src/components/safetyHome.jsx
+++ my-test-app/src/components/safetyHome.jsx
@@ -5,135 +5,14 @@ import ButtonBase from "@material-ui/core/ButtonBase";
 import Typography from "@material-ui/core/Typography";
 import ImageDatabase from "../db.js";
 import Button from "@material-ui/core/Button";
-import ButtonBases from './ButtonBases';
+import ButtonBases from "./ButtonBases";
 import background1 from "./pictures/textbooks.jpg";
 import background2 from "./pictures/FESlogo.jpg";
 import background3 from "./pictures/Man-filling-up-checklist.jpg";
 
-<<<<<<< HEAD
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    minWidth: 300,
-    width: "100%"
-  },
-  image: {
-    position: "relative",
-    height: 200,
-    [theme.breakpoints.down("xs")]: {
-      width: "100% !important", // Overrides inline-style
-      height: 100
-    },
-    "&:hover, &$focusVisible": {
-      zIndex: 1,
-      "& $imageBackdrop": {
-        opacity: 0.15
-      },
-      "& $imageMarked": {
-        opacity: 0
-      },
-      "& $imageTitle": {
-        border: "4px solid currentColor"
-      }
-    }
-  },
-  focusVisible: {},
-  imageButton: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    color: theme.palette.common.white
-  },
-  imageSrc: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundSize: "cover",
-    backgroundPosition: "center 40%"
-  },
-  imageBackdrop: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundColor: theme.palette.common.black,
-    opacity: 0.4,
-    transition: theme.transitions.create("opacity")
-  },
-  imageTitle: {
-    position: "relative",
-    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
-      6}px`
-  },
-  imageMarked: {
-    height: 3,
-    width: 18,
-    backgroundColor: theme.palette.common.white,
-    position: "absolute",
-    bottom: -2,
-    left: "calc(50% - 9px)",
-    transition: theme.transitions.create("opacity")
-  }
-}));
-
-function ButtonBases(name, link, url) {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <ButtonBase
-        focusRipple
-        key={name}
-        component={Link}
-        to={`${link}`}
-        className={classes.image}
-        focusVisibleClassName={classes.focusVisible}
-        style={{
-          width: "100%"
-        }}
-      >
-        <span
-          className={classes.imageSrc}
-          style={{
-            backgroundImage: `url(${url})`
-          }}
-        />
-        <span className={classes.imageBackdrop} />
-        <span className={classes.imageButton}>
-          <Typography
-            component="span"
-            variant="subtitle1"
-            color="inherit"
-            className={classes.imageTitle}
-            np
-          >
-            {name}
-            <span className={classes.imageMarked} />
-          </Typography>
-        </span>
-      </ButtonBase>
-    </div>
-  );
-}
-
 function doLink(thing) {
   return (window.location = thing);
 }
-
-=======
-function doLink(thing){
-  return window.location = thing;
- }
->>>>>>> master
 function SafetyPage(props) {
   const [mainMenu, setMenu] = useState([
     {
