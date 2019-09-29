d4703ea348d810d38c8e7db4dba08ed127f03ef5
limg2 <limg2@student.unimelb.edu.au>
2019-09-29 19:59:02 +1000

    ButtonBases now works with the quiz api

--- my-test-app/src/components/home.jsx
+++ my-test-app/src/components/home.jsx
@@ -10,6 +10,78 @@ import background1 from "./pictures/questionmarks.png";
 import background2 from "./pictures/FESlogo.jpg";
 import background3 from "./pictures/building.jpg";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    minWidth: 300,
+    width: '100%',
+  },
+  image: {
+    position: 'relative',
+    height: 300,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15,
+      },
+      '& $imageMarked': {
+        opacity: 0,
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor',
+      },
+    },
+  },
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  },
+}));
 
 function doLink(thing){
   return window.location = thing;
@@ -17,6 +89,8 @@ function doLink(thing){
 }
 
 function HomePage(props) {
+  const classes = useStyles();
+
   const [mainMenu, setMenu] = useState([
     { name: "Fire Safety Information", link: "./safetyHome", url: background3 },
     { name: "Quizzes", link: "./quizzes", url: background1 },
@@ -28,7 +102,7 @@ function HomePage(props) {
     <React.Fragment>
       <h1>Home</h1>
       {mainMenu.map(option =>
-        ButtonBases(option.name, option.link, option.url, doLink)
+        ButtonBases(option.name, option.link, option.url, doLink, classes)
       )}
       {/* <button onClick={function() { doLink('./thing'); }} >asdf</button> */}
     </React.Fragment>
